466d9cc2a6e6af81d82bdd6f813948383d63be7e
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-14 20:10:53 +0530

    Updates

---
+++ precache-manifest.4ab2bab10806a7c9fbd5e574c2df1976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26306a7451f0c74330dcb6641cfab434",
+    "url": "/bipolar-challenge/index.html"
+  },
+  {
+    "revision": "bb86fa093ac03ca6874c",
+    "url": "/bipolar-challenge/static/css/main.f55ac132.chunk.css"
+  },
+  {
+    "revision": "c63ae724505d09d9630e",
+    "url": "/bipolar-challenge/static/js/2.9cb2044c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bipolar-challenge/static/js/2.9cb2044c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb86fa093ac03ca6874c",
+    "url": "/bipolar-challenge/static/js/main.60e5879a.chunk.js"
+  },
+  {
+    "revision": "2aa1254e9ba336254c38",
+    "url": "/bipolar-challenge/static/js/runtime-main.6b888c26.js"
+  },
+  {
+    "revision": "c987ff1f698b9ab55637b2449e65bec5",
+    "url": "/bipolar-challenge/static/media/loader.c987ff1f.gif"
+  }
+]);
